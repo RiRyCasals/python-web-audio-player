5aa80f0044299153ccbdbecff54bb3dd7f9b576e
RiRyCasals <rirycasals@gmail.com>
2022-04-03 23:25:01 +0900

    お試しストリーミング

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, Response
 
+dir_name = os.path.dirname(__file__)
 file_path = '/music/example.mp3'
+buffer_size = 1024
 
 app = Flask(__name__)
 
@@ -11,8 +13,15 @@ def index():
                            file_path=file_path)
 
 @app.route('/music/<path:file_name>')
-def send_audio_src(file_name):
-    return send_from_directory('music', file_name)
+def stream_audio(file_name):
+    print(dir_name + file_name)
+    def generate():
+        with open(dir_name + '/music/' + file_name, 'rb') as f:
+            data = f.read(buffer_size)
+            while data:
+                yield data
+                data = f.read(buffer_size)
+    return Response(generate(), mimetype='audio/x-wav')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
