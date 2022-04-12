48b4d84c1e3febea1012fc34f4a8ed27a63e6c58
RiRyCasals <rirycasals@gmail.com>
2022-04-12 20:51:03 +0900

    init files

--- src/app.py
+++ src/app.py
@@ -1,27 +1,23 @@
 import os
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, redirect
 
 dir_name = os.path.dirname(__file__)
-file_path = '/music/example.mp3'
-buffer_size = 1024
 
 app = Flask(__name__)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+#login require
 @app.route('/')
 def index():
-    return render_template('index.html',
-                           file_path=file_path)
+    return redirect('/home')
 
-@app.route('/music/<path:file_name>')
-def stream_audio(file_name):
-    print(dir_name + file_name)
-    def generate():
-        with open(dir_name + '/music/' + file_name, 'rb') as f:
-            data = f.read(buffer_size)
-            while data:
-                yield data
-                data = f.read(buffer_size)
-    return Response(generate(), mimetype='audio/x-wav')
+#login require
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
