1699c7f096b039f3bc1c2ba9dad7058ab418104d
RiRyCasals <rirycasals@gmail.com>
2022-04-15 18:10:18 +0900

    50分のファイルを読み込めるかテスト

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, send_from_directory
 
 dir_name = os.path.dirname(__file__)
+music_path = dir_name + '/music/sample.wav'
 
 app = Flask(__name__)
 
@@ -17,7 +18,11 @@ def index():
 #login require
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', music_path=music_path)
+
+@app.route(dir_name + '/music/<path>')
+def send_audio_data(path):
+    return send_from_directory('music', path)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
